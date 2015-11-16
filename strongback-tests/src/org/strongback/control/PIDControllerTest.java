3f6ceda2f5252e9ebf505cc1efb53dcb37070473
Randall Hauch <rhauch@gmail.com>
2015-11-15 22:17:13 -0600

    PIDController now supports multiple named gain profiles

--- strongback-tests/src/org/strongback/control/PIDControllerTest.java
+++ strongback-tests/src/org/strongback/control/PIDControllerTest.java
@@ -20,6 +20,7 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.strongback.control.PIDController.Gains;
 import org.strongback.function.DoubleBiFunction;
 
 import edu.wpi.first.wpilibj.HLUsageReporting;
@@ -46,7 +47,7 @@ public class PIDControllerTest {
 
         public void setValue(double input) {
             double newValue = model.applyAsDouble(actualValue, input);
-            if ( print ) System.out.println("actual=" + actualValue + "; input=" + input + "; newValue=" + newValue);
+            if (print) System.out.println("actual=" + actualValue + "; input=" + input + "; newValue=" + newValue);
             this.actualValue = newValue;
         }
     }
@@ -74,14 +75,14 @@ public class PIDControllerTest {
     @Test
     public void shouldUseProportionalOnly() {
         model = simple();
-        //model.print = true;
+        // model.print = true;
         controller = new PIDController(model::getActualValue, model::setValue).withGains(0.9, 0.0, 0.0)
                                                                               .inputRange(-1.0, 1.0)
                                                                               .outputRange(-1.0, 1.0)
                                                                               .tolerance(0.02)
                                                                               .setpoint(0.5);
         assertThat(runController(10)).isLessThan(5);
-        assertThat(model.getActualValue()-0.5 < 0.02).isTrue();
+        assertThat(model.getActualValue() - 0.5 < 0.02).isTrue();
     }
 
     @Test
@@ -94,47 +95,71 @@ public class PIDControllerTest {
                                                                               .tolerance(0.02)
                                                                               .setpoint(0.5);
         assertThat(runController(10)).isLessThan(5);
-        assertThat(model.getActualValue()-0.5 < 0.02).isTrue();
+        assertThat(model.getActualValue() - 0.5 < 0.02).isTrue();
     }
-
 
     @Test
     public void shouldUseProportionalOnlyWithInitialValue() {
         model = simple();
         model.setValue(0.2);
-        //model.print = true;
+        // model.print = true;
         controller = new PIDController(model::getActualValue, model::setValue).withGains(0.9, 0.0, 0.0)
                                                                               .inputRange(-1.0, 1.0)
                                                                               .outputRange(-1.0, 1.0)
                                                                               .tolerance(0.02)
                                                                               .setpoint(0.5);
         assertThat(runController(10)).isLessThan(5);
-        assertThat(model.getActualValue()-0.5 < 0.02).isTrue();
+        assertThat(model.getActualValue() - 0.5 < 0.02).isTrue();
     }
 
     @Test
     public void shouldUseProportionalAndDifferentialWithInitialValue() {
         model = simple();
         model.setValue(0.2);
-        //model.print = true;
+        // model.print = true;
         controller = new PIDController(model::getActualValue, model::setValue).withGains(0.7, 0.0, 0.3)
                                                                               .inputRange(-1.0, 1.0)
                                                                               .outputRange(-1.0, 1.0)
                                                                               .tolerance(0.02)
                                                                               .setpoint(0.5);
         assertThat(runController(10)).isLessThan(5);
-        assertThat(model.getActualValue()-0.5 < 0.02).isTrue();
+        assertThat(model.getActualValue() - 0.5 < 0.02).isTrue();
+    }
+
+    @Test
+    public void shouldDefineMultipleProfiles() {
+        model = simple();
+        model.setValue(0.2);
+        // model.print = true;
+        controller = new PIDController(model::getActualValue, model::setValue).withGains(0.9, 0.0, 0.0)
+                                                                              .withProfile("two", 1.2, 0.0, 2.0)
+                                                                              .withProfile("three", 1.3, 0.0, 3.0)
+                                                                              .inputRange(-1.0, 1.0)
+                                                                              .outputRange(-1.0, 1.0)
+                                                                              .tolerance(0.02)
+                                                                              .setpoint(0.5);
+        assertThat(controller.getProfileNames()).containsOnly(PIDController.DEFAULT_PROFILE,"two","three");
+        assertThat(controller.getCurrentProfile()).isEqualTo(PIDController.DEFAULT_PROFILE);
+        assertGains(controller.getGainsForCurrentProfile(), 0.9, 0.0, 0.0, 0.0);
+        assertGains(controller.getGainsFor("two"), 1.2, 0.0, 2.0, 0.0);
+        assertGains(controller.getGainsFor("three"), 1.3, 0.0, 3.0, 0.0);
+        controller.useProfile("two");
+        assertGains(controller.getGainsForCurrentProfile(), 1.2, 0.0, 2.0, 0.0);
+        controller.useProfile("three");
+        assertGains(controller.getGainsForCurrentProfile(), 1.3, 0.0, 3.0, 0.0);
     }
 
     @Test
     public void shouldUseProportionalOnlyWPILib() throws InterruptedException {
-        HLUsageReporting.SetImplementation(new HLUsageReporting.Interface(){
+        HLUsageReporting.SetImplementation(new HLUsageReporting.Interface() {
             @Override
             public void reportPIDController(int num) {
             }
+
             @Override
             public void reportScheduler() {
             }
+
             @Override
             public void reportSmartDashboard() {
             }
@@ -150,16 +175,23 @@ public class PIDControllerTest {
         wpi.enable();
         Thread.sleep(300);
         wpi.disable();
-        assertThat(model.getActualValue()-0.5 < 0.02).isTrue();
+        assertThat(model.getActualValue() - 0.5 < 0.02).isTrue();
     }
 
-    protected int runController( int maxNumSteps) {
+    protected int runController(int maxNumSteps) {
         int counter = 0;
-        while ( !controller.computeOutput() && counter < maxNumSteps) {
+        while (!controller.computeOutput() && counter < maxNumSteps) {
             ++counter;
-            if ( print ) System.out.println(counter + " " + model.getActualValue());
+            if (print) System.out.println(counter + " " + model.getActualValue());
         }
         return counter;
+    }
+
+    protected void assertGains( Gains gains, double p, double i, double d, double f ) {
+        assertThat(gains.getP()).isEqualTo(p);
+        assertThat(gains.getI()).isEqualTo(i);
+        assertThat(gains.getD()).isEqualTo(d);
+        assertThat(gains.getFeedForward()).isEqualTo(f);
     }
 
 }
